c08dcc7d32ec9e7516f12ff1a7315051134a02b6
Alamiro <alamiro.vera@icloud.com>
2022-11-16 11:46:55 -0300

    fix route

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: 'https://alamiro.github.io/',
+  base: 'https://alamiro.github.io/gh-react-portafolio/',
   plugins: [react()]
 })
